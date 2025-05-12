525eded4a3fbe47ff9387b7419fd2594d9c773ed
borroot <inbox@borroot.com>
2025-05-13 00:14:43 +0200

    Work on ppo results

--- scripts/env-download.py
+++ scripts/env-download.py
@@ -4,11 +4,14 @@ import grid2op
 
 grid2op.change_local_dir(os.path.join(os.getcwd(), "envs"))
 
-env_name = "l2rpn_case14_sandbox"
+# env_name = "l2rpn_case14_sandbox"
+# grid2op.make(env_name)
+
+# env_name = "l2rpn_neurips_2020_track1_small"
+# grid2op.make(env_name)
+
+env_name = "l2rpn_neurips_2020_track1_large"
 grid2op.make(env_name)
 
-env_name = "l2rpn_neurips_2020_track1_small"
-grid2op.make(env_name)
-
-env_name = "l2rpn_wcci_2022"
-grid2op.make(env_name)
+# env_name = "l2rpn_wcci_2022"
+# grid2op.make(env_name)
